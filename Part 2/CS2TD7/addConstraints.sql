d61990748893522c711c96f58b0e96a1c6c67546
IanField90 <ian.field90@gmail.com>
2011-03-11 14:24:12 +0000

    Added all non-trigger constraints

--- Part 2/CS2TD7/addConstraints.sql
+++ Part 2/CS2TD7/addConstraints.sql
@@ -4,156 +4,165 @@ CREATE SEQUENCE company_sq INCREMENT BY 1 START WITH 1 NOMAXVALUE NOCYCLE CACHE 
 CREATE SEQUENCE contact_sq INCREMENT BY 1 START WITH 1 NOMAXVALUE NOCYCLE CACHE 10;
 CREATE SEQUENCE placement_sq INCREMENT BY 1 START WITH 1 NOMAXVALUE NOCYCLE CACHE 10;
 
--- Add simple constraints to tables 
+-- Add more simple constraints to tables
 ALTER TABLE student
 	ADD(
-		CONSTRAINT student_pk PRIMARY KEY (StudentID),
-		CONSTRAINT student_no CHECK (
-			regexp_like(StudentNo, '[:alpha:]{3}[:digit:]{2}[:alpha:]{3}')),
-		CONSTRAINT student_title CHECK (
-			Title IN ('MR','MRS','DR', 'PROF', 'MISS', 'MS', 'OTHER')),
-		CONSTRAINT student_email CHECK (regexp_like(email,'\W@\W')
-	)
+    CONSTRAINT student_pk PRIMARY KEY (StudentID),
+    CONSTRAINT student_no CHECK (
+      regexp_like(StudentNumber, '[:alpha:]{3}[:digit:]{2}[:alpha:]{3}')),
+    CONSTRAINT student_title CHECK (
+      Title IN ('MR','MRS','DR', 'PROF', 'MISS', 'MS', 'OTHER')),
+    CONSTRAINT student_email CHECK (regexp_like(EmailAddress,'\W@\W'))
+  )
 ;
 
 ALTER TABLE company
-	ADD(
-		CONSTRAINT company_pk PRIMARY KEY (CompanyID),
-		CONSTRAINT company_uq UNIQUE(CompanyID, Sector),
-		CONSTRAINT company_sz CHECK ('Size' > 5),
-		CONSTRAINT company_sect CHECK (
-			Sector IN ('Comp', 'Finance', 'Edu', 'Public', 'Other')
-		)
-	)
+  ADD(
+    CONSTRAINT company_pk PRIMARY KEY (CompanyID),
+    CONSTRAINT company_uq UNIQUE(CompanyName, Sector),
+    CONSTRAINT company_size CHECK ('Size' > '5'),
+    CONSTRAINT company_sect CHECK (
+      'Sector' IN ('Comp', 'Finance', 'Edu', 'Public', 'Other')
+    )
+  )
 ;
 
 ALTER TABLE contact
-	ADD(
-		CONSTRAINT contact_pk PRIMARY KEY (ContactID),
-		CONSTRAINT contact_fk FOREIGN KEY (CompanyID) REFERENCES company(CompanyID),
-		CONSTRAINT contact_email CHECK (regexp_like(email, '\W@\W'),
-		CONSTRAINT contact_uq UNIQUE(email),
-		CONSTRAINT contact_title CHECK (
-			Title IN ('MR','MRS','DR', 'PROF', 'MISS', 'MS', 'OTHER')
-		)
-	)
+  ADD(
+    CONSTRAINT contact_pk PRIMARY KEY (ContactID),
+    CONSTRAINT contact_fk FOREIGN KEY (CompanyID) REFERENCES company(CompanyID),
+    CONSTRAINT contact_email CHECK (regexp_like(email, '\W@\W')),
+    CONSTRAINT contact_uq UNIQUE(email),
+    CONSTRAINT contact_title CHECK (
+      Title IN ('MR','MRS','DR', 'PROF', 'MISS', 'MS', 'OTHER')
+    )
+  )
 ;
 
+--Date Error
 ALTER TABLE placement
-	ADD(
-		CONSTRAINT placement_pk PRIMARY KEY (PlacementID),
-		CONSTRAINT placement_nopos CHECK (NoOfPositions > 0),
-		CONSTRAINT placement_len CHECK (LengthMonths >= 3 AND LengthMonths <=15),
-		CONSTRAINT placement_sal CHECK (Salary >=0 AND Salary <= 99999.99),
-		CONSTRAINT placement_dl CHECK (
-			deadline > sysdate AND deadline <= (sysdate + interval '1' year)
-		),
-		CONSTRAINT placement_sd CHECK (StartDate > (deadline + interval '2' years))
-	)
+  ADD(
+    CONSTRAINT placement_pk PRIMARY KEY (PlacementID),
+    CONSTRAINT placement_nopos CHECK (NoOfPositions > 0),
+    CONSTRAINT placement_len CHECK (LengthMonths >= 3 AND LengthMonths <=15),
+    CONSTRAINT placement_sal CHECK (Salary >=0 AND Salary <= 99999.99),
+    CONSTRAINT placement_sd CHECK (StartDate > (deadline + 730))
+  )
 ;
 
 ALTER TABLE placement_contacts
-	ADD(
-		CONSTRAINT placements_contacts_pk PRIMARY KEY (PlacementID AND ContactID),
-		CONSTRAINT placements_contacts_fk1 FOREIGN KEY (PlacementID)
-			REFERENCES placement(PlacementID),
-		CONSTRAINT placements_contacts_fk2 FOREIGN KEY (ContactID)
-			REFERENCES contact(ContactID)
-	)
+  ADD(
+    CONSTRAINT placement_contacts_pk PRIMARY KEY (PlacementID, ContactID),
+    CONSTRAINT placement_contacts_fk1 FOREIGN KEY (PlacementID)
+      REFERENCES placement(PlacementID),
+    CONSTRAINT placement_contacts_fk2 FOREIGN KEY (ContactID)
+      REFERENCES contact(ContactID)
+  )
 ;
 
 ALTER TABLE application
-	ADD(
-		CONSTRAINT application_pk PRIMARY KEY (ApplicationID),
-		CONSTRAINT application_fk1 FOREIGN KEY (StudentID)
-			REFERENCES student(StudentID),
-		CONSTRAINT application_fk2 FOREIGN KEY (PlacementID)
-			REFERENCES placement(PlacementID),
-		CONSTRAINT application_dte CHECK (
-			DateOfApplication <= (SELECT Deadline FROM placement 
-			WHERE placmentID = application.PlacementID
-		),
-		CONSTRAINT application_stat CHECK (Status IN ('C','A','T','U','R'))
-	)
+  ADD(
+    CONSTRAINT application_pk PRIMARY KEY (ApplicationID),
+    CONSTRAINT application_fk1 FOREIGN KEY (StudentID)
+      REFERENCES student(StudentID),
+    CONSTRAINT application_fk2 FOREIGN KEY (PlacementID)
+      REFERENCES placement(PlacementID),
+    CONSTRAINT application_stat CHECK (Status IN ('C','A','T','U','R'))
+  )
 ;
 
 ALTER TABLE stage_history
-	ADD(
-		CONSTRAINT stage_hist_pk PRIMARY KEY (ApplicationID AND StageDate),
-		CONSTRAINT stage_hist_fk FOREIGN KEY (ApplicationID) 
-			REFERENCES application(ApplicationID)
-	)
+  ADD(
+    CONSTRAINT stage_hist_pk PRIMARY KEY (ApplicationID, StageDate),
+    CONSTRAINT stage_hist_fk FOREIGN KEY (ApplicationID) 
+      REFERENCES application(ApplicationID)
+  )
 ;
 
+/*
 
--- #### Complex constraints ###
+-- application date check
+    /*
+    CONSTRAINT application_date CHECK (
+      DateOfApplication <= (SELECT Deadline FROM placement 
+      WHERE placmentID = application.PlacementID
+    ),*/
+    
+-- placement deadline date check
+--CONSTRAINT placement_dl CHECK (deadline BETWEEN sysdate AND (dealine + 365))
+
 -- application update constraints
 CREATE OR REPLACE TRIGGER application_update
-	BEFORE UPDATE ON application
-	FOR EACH ROW
-	BEGIN
-	IF(:NEW.Status = 'C' OR :NEW.Status = 'A')
-	THEN
-		--If stage_history has C or A under status for placementID
-		IF(NUM_ROWS(SELECT Stage FROM stage_history 
-			WHERE PlacementID = NEW.PlacementID AND Status = NEW.Status) = 1)
-			--Block 'C' and 'A'
-			NULL
-		ELSE
-			--Do update
-			UPDATE TABLE application
-			SET Status = :NEW.Status
-			WHERE PlacementID = :NEW.PlacementID
-
-			--Create Stage_history entry
-			INSERT INTO stage_history
-			VALUES(
-			:NEW.ApplicationID, 
-			:NEW.StageDate, 
-			:NEW.CompanyUpdate, 
-			:NEW.StudentUpdate,
-			:NEW.Stage,
-			:NEW.Status
-			);
-		END IF;
-	ELSE
-		--Do update regardless
-
-		--Create Stage_history entry
-		INSERT INTO stage_history
-		VALUES(
-		:NEW.ApplicationID, 
-		:NEW.StageDate, 
-		:NEW.CompanyUpdate, 
-		:NEW.StudentUpdate,
-		:NEW.Stage,
-		:NEW.Status
-		);
-	END IF;
-END;
+  BEFORE UPDATE ON application
+  FOR EACH ROW
+  BEGIN
+    IF(:NEW.Status = 'C' OR :NEW.Status = 'A')
+      THEN
+        --If stage_history has C or A under status for placementID
+        IF(NUM_ROWS(SELECT Stage FROM stage_history 
+            WHERE PlacementID = NEW.PlacementID AND Status = NEW.Status) = 1)
+          --Block 'C' and 'A'
+          NULL
+        ELSE
+          --Do update
+          UPDATE TABLE application
+          SET Status = :NEW.Status
+          WHERE PlacementID = NEW.PlacementID
+          
+          --Create Stage_history entry
+          INSERT INTO stage_history
+          VALUES(
+          :NEW.ApplicationID, 
+          :NEW.StageDate, 
+          :NEW.CompanyUpdate, 
+          :NEW.StudentUpdate,
+          :NEW.Stage,
+          :NEW.Status
+          );
+        END IF;
+    ELSE
+      --Do update regardless
+      
+      --Create Stage_history entry
+      INSERT INTO stage_history
+      VALUES(
+      :NEW.ApplicationID, 
+      :NEW.StageDate, 
+      :NEW.CompanyUpdate, 
+      :NEW.StudentUpdate,
+      :NEW.Stage,
+      :NEW.Status
+      );
+    END IF;
+  END;
 
 -- Application insert constraint
 -- Student cannot have more than one appication 'C'
 CREATE OR REPLACE TRIGGER application_insert
-BEFORE INSERT ON application
-FOR EACH ROW
-BEGIN
-	--If student already has placementID with status 'A' block insert
-	IF(NUM_ROWS(SELECT * FROM application WHERE StudentID = NEW.StudentID AND Status = 'A') = 1)
-		THEN
-			NULL
-		ELSE
-		-- Perform Insertion
-	END IF;
-
-	--Create Stage_history entry
-	INSERT INTO stage_history
-	VALUES(
-	:NEW.ApplicationID, 
-	:NEW.StageDate, 
-	:NEW.CompanyUpdate, 
-	:NEW.StudentUpdate,
-	:NEW.Stage,
-	:NEW.Status
-	);
-END;
+  BEFORE INSERT ON application
+  FOR EACH ROW
+  BEGIN
+    --If student already has placementID with status 'A' block insert
+    IF(NUM_ROWS(SELECT * FROM application WHERE StudentID = NEW.StudentID AND Status = 'A') = 1)
+      THEN
+        NULL
+      ELSE
+      -- Perform Insertion
+      INSERT INTO application
+      VALUES(
+      
+      );
+    END IF;
+  
+    --Create Stage_history entry
+    INSERT INTO stage_history
+    VALUES(
+    :NEW.ApplicationID, 
+    :NEW.StageDate, 
+    :NEW.CompanyUpdate, 
+    :NEW.StudentUpdate,
+    :NEW.Stage,
+    :NEW.Status
+    );
+  END;
+  */
--- Part 2/CS2TD7/dropConstraints.sql
+++ Part 2/CS2TD7/dropConstraints.sql
@@ -1,52 +1,46 @@
-ALTER TRIGGER application_insert DISABLE;
-ALTER TRIGGER application_update DISABLE;
+--ALTER TRIGGER application_insert DISABLE;
+--ALTER TRIGGER application_update DISABLE;
 
-ALTER TABLE student
-	DROP CONSTRAINT student_pk,
-	DROP CONSTRAINT student_no,
-	DROP CONSTRAINT student_title,
-	DROP CONSTRAINT student_email
-;
+DROP SEQUENCE student_sq;
+DROP SEQUENCE company_sq;
+DROP SEQUENCE contact_sq;
+DROP SEQUENCE placement_sq;
 
-ALTER TABLE company
-	DROP CONSTRAINT company_pk,
-	DROP CONSTRAINT company_uq,
-	DROP CONSTRAINT company_size,
-	DROP CONSTRAINT company_sect
-;
+-- Drop foreign keys first
+ALTER TABLE application DROP CONSTRAINT application_fk1;
+ALTER TABLE application DROP CONSTRAINT application_fk2;
+ALTER TABLE placement_contacts DROP CONSTRAINT placement_contacts_fk1;
+ALTER TABLE placement_contacts DROP CONSTRAINT placement_contacts_fk2;
+ALTER TABLE stage_history DROP CONSTRAINT stage_hist_fk;
+ALTER TABLE contact DROP CONSTRAINT contact_fk;
 
-ALTER TABLE contact
-	DROP CONSTRAINT contact_pk,
-	DROP CONSTRAINT contact_fk,
-	DROP CONSTRAINT contact_email,
-	DROP CONSTRAINT contact_uq,
-	DROP CONSTRAINT contact_title
-;
+ALTER TABLE student DROP CONSTRAINT student_pk;
+ALTER TABLE student DROP CONSTRAINT student_no;
+ALTER TABLE student DROP CONSTRAINT student_title;
+ALTER TABLE student DROP CONSTRAINT student_email;
 
-ALTER TABLE placement
-	DROP CONSTRAINT placement_pk,
-	DROP CONSTRAINT placement_nopos,
-	DROP CONSTRAINT placement_len,
-	DROP CONSTRAINT placement_sal,
-	DROP CONSTRAINT placement_dl,
-	DROP CONSTRAINT placement_sd
-;
+ALTER TABLE company DROP CONSTRAINT company_uq;
+ALTER TABLE company DROP CONSTRAINT company_size;
+ALTER TABLE company DROP CONSTRAINT company_sect;
 
-ALTER TABLE placement_contacts
-	DROP CONSTRAINT placement_contacts_pk,
-	DROP CONSTRAINT placement_contacts_fk1,
-	DROP CONSTRAINT placement_contacts_fk2
-;
+ALTER TABLE contact DROP CONSTRAINT contact_pk;
+ALTER TABLE contact DROP CONSTRAINT contact_email;
+ALTER TABLE contact DROP CONSTRAINT contact_uq;
+ALTER TABLE contact  DROP CONSTRAINT contact_title;
 
-ALTER TABLE application
-	DROP CONSTRAINT application_pk,
-	DROP CONSTRAINT application_fk1,
-	DROP CONSTRAINT application_fk2,
-	DROP CONSTRAINT application_date,
-	DROP CONSTRAINT application_stat
-;
+ALTER TABLE placement DROP CONSTRAINT placement_pk;
+ALTER TABLE placement DROP CONSTRAINT placement_nopos;
+ALTER TABLE placement DROP CONSTRAINT placement_len;
+ALTER TABLE placement DROP CONSTRAINT placement_sal;
+ALTER TABLE placement DROP CONSTRAINT placement_dl;
+ALTER TABLE placement DROP CONSTRAINT placement_sd;
 
-ALTER TABLE stage_history
-	DROP CONSTRAINT stage_hist_pk,
-	DROP CONSTRAINT stage_hist_fk
-;
+ALTER TABLE placement_contacts DROP CONSTRAINT placement_contacts_pk;
+
+ALTER TABLE application DROP CONSTRAINT application_pk;
+ALTER TABLE application DROP CONSTRAINT application_date;
+ALTER TABLE application DROP CONSTRAINT application_stat;
+
+ALTER TABLE stage_history DROP CONSTRAINT stage_hist_pk;
+
+ALTER TABLE company DROP CONSTRAINT company_pk;
