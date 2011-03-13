df3db022511fe8ca001829cfd1036df99b669d30
IanField90 <ian.field90@gmail.com>
2011-03-13 12:30:43 +0000

    Possibly 2.c complete.

--- Part 2/CS2TD7/package.sql
+++ Part 2/CS2TD7/package.sql
@@ -34,10 +34,33 @@ AS
 	PROCEDURE delete_comp(p_id company.companyid%TYPE)
 	IS
 	BEGIN
-		--ensure references are deleted too ON DELETE CASCADE for FOREIGN KEYs
+		--to ensure references are deleted too ON DELETE CASCADE for FOREIGN KEYs
 		DELETE * FROM comp 
 		WHERE companyid = p_id;
 	END delete_comp;
+
+  --Return placements with no applicaitions
+  PROCEDURE query_nil_app_placements()
+  IS
+  DECLARE
+    CURSOR query_cur IS SELECT * FROM Application;
+    NUMBER x;
+  
+  BEGIN
+    OPEN query_cur;
+    LOOP
+      X = SELECT COUNT(*) INTO X
+          FROM Application 
+          WHERE PlacementID = query_cur.PlacementID;
+                  
+      IF X = 0
+        FETCH query_cur INTO temp;
+      END IF;
+    END LOOP;
+  END query_nil_app_placements;
+
+
+--  PROCEDURE update_row()
 
 END placement pck;
 /
