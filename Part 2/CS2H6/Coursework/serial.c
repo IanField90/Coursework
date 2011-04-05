2f458fc121938caeb9aa61646864324263bdda3e
IanField90 <ian.field90@gmail.com>
2011-04-05 11:48:52 +0100

    Added comments for serial.

--- Part 2/CS2H6/Coursework/serial.c
+++ Part 2/CS2H6/Coursework/serial.c
@@ -13,7 +13,8 @@
 #define RIGHT_TEMP 100
 
 //2D array of reservations, calloc inits to 0
-double** makeBuff(int columns, int rows){
+//returns NULL if at any stage allocation fails
+double** makeBuff(int rows, int columns){
 	int i;
 	double** ret;
 	ret = (double**) calloc(rows, sizeof(double*));
@@ -41,23 +42,29 @@ int main(int argc, char **argv) {
 	double curVal, topVal, leftVal, rightVal, bottomVal;
 	double **grid, **grid_new;//, **temp;
 	int flag=1;
-	time_t t1, t2;
-	grid = makeBuff(NUM_COLUMNS, NUM_ROWS);
-	grid_new = makeBuff(NUM_COLUMNS, NUM_ROWS);
+	time_t t1, t2;//used to time the application
+	//reserve the grid in memory
+	//referenced in C convention order array[rows][columns]
+	grid = makeBuff(NUM_ROWS, NUM_COLUMNS);
+	grid_new = makeBuff(NUM_ROWS, NUM_COLUMNS);
+	
+	//should memory allocation fail at any stage the program should display an error and exit
 	if(grid == NULL || grid_new == NULL){
 		printf("Memory allocation failure. Fatal!\n");
-		return -1;
+		return -1;//return status -1 to inform operating system the program did not execute as expected.
 	}
 	time(&t1);
-	//j column, k row
-	//optimise K
+	
+	//k row, j column, optimised loop with greater iterations inernally
 	for(i = 0; i < NUM_TIME_STEPS; i++){
 		for(k = 0; k < NUM_ROWS; k++){	
 			for(j = 0; j < NUM_COLUMNS; j++){			
 				//Do calculation
 				if(k==0){
+					//top row so use ambient top value
 					topVal = TOP_TEMP;
 				}else{
+					//internal top value
 					if(flag==1){
 						topVal = grid[k-1][j];
 					}else{
@@ -66,8 +73,10 @@ int main(int argc, char **argv) {
 				}
 				
 				if(k==NUM_ROWS-1){
+					//bottom row so use ambient bottom value
 					bottomVal = BOTTOM_TEMP;
 				}else{
+					//internal bottom value
 					if(flag==1){
 						bottomVal = grid[k+1][j];
 					}else{
@@ -76,9 +85,10 @@ int main(int argc, char **argv) {
 				}
 				
 				if(j==0){
-					leftVal = LEFT_TEMP;//only on left node
+					//left column so use ambient left value
+					leftVal = LEFT_TEMP;
 				}else{
-					//left inner
+					//internal left value
 					if(flag==1){
 						leftVal = grid[k][j-1];
 					}else{
@@ -88,35 +98,34 @@ int main(int argc, char **argv) {
 				
 				
 				if(j==NUM_COLUMNS-1){
-					rightVal = RIGHT_TEMP;//only on right node
+					//right column so use ambient right value
+					rightVal = RIGHT_TEMP;
 				}else{
+					//internal right value
 					if(flag==1){
 						rightVal = grid[k][j+1];
 					}else{
 						rightVal = grid_new[k][j+1];
 					}
 				}
-				
+				//do actual calculation. Inline has reduced overhead and more efficiency than function call.
 				if(flag==1){
 					curVal = grid[k][j];
-					grid_new[k][j] = (curVal+topVal+leftVal+bottomVal+rightVal) / 5;//calcTemp(curVal, topVal, leftVal, bottomVal, rightVal);
+					grid_new[k][j] = (curVal+topVal+leftVal+bottomVal+rightVal) / 5;
 				}
 				else{
 					curVal = grid_new[k][j];
-					grid[k][j] = (curVal+topVal+leftVal+bottomVal+rightVal) / 5;//calcTemp(curVal, topVal, leftVal, bottomVal, rightVal);
+					grid[k][j] = (curVal+topVal+leftVal+bottomVal+rightVal) / 5;
 				}
 				
 			}
 		}
-//		for(j = 0; j < NUM_COLUMNS; j++){
-//			for(k = 0; k < NUM_ROWS; k++){
-//				grid[k][j] = grid_new[k][j];
-//			}
-//		}
-		if (flag == 1) flag = 0; else flag = 1;
-//		temp = grid; 
-//		grid = grid_new; 
-//		grid_new = temp;
+		//used to switch T and T-1
+		if (flag == 1){
+			flag = 0; 
+		}else{ 
+			flag = 1;
+		}
 	}
 	time(&t2);
 	printf("Time taken: %d seconds.\n", (int)(t2-t1));
@@ -133,6 +142,8 @@ int main(int argc, char **argv) {
 	printf("End of Results!\n");
 	 */
 	
+	//double failsafe for freeing memory
+	//Free memory
 	if(grid_new != NULL){
 		for(i = 0; i < NUM_ROWS; i++){
 			if(grid_new[i] != NULL){
@@ -141,6 +152,7 @@ int main(int argc, char **argv) {
 		}
 		free(grid_new);
 	}
+	//free memory
 	if(grid != NULL){
 		//Free memory again
 		for(i = 0; i < NUM_ROWS; i++){
@@ -150,5 +162,6 @@ int main(int argc, char **argv) {
 		}
 		free(grid);
 	}
+	//inform the operating system that the program executed as expected
 	return 0;
 }
