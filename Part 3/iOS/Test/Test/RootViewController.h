4ac7b693a7e3cd73fcaa9448e3d2ca15fc4966dd
IanField90 <ian.field90@gmail.com>
2011-06-23 10:50:16 +0100

    Add some FYP stuff, including mock Quiz UI for WAGGGS meeting

---
+++ Part 3/iOS/Test/Test/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Test
+//
+//  Created by Ian Field on 17/06/2011.
+//  Copyright 2011 University of Reading Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ Part 3/iOS/Test/TestTests/TestTests.h
@@ -0,0 +1,17 @@
+//
+//  TestTests.h
+//  TestTests
+//
+//  Created by Ian Field on 17/06/2011.
+//  Copyright 2011 University of Reading Student. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface TestTests : SenTestCase {
+@private
+    
+}
+
+@end
