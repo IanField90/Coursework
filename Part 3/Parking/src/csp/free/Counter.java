3acfe0070b3ba2dbe4b586749b129c3fe1f76e38
Ian Field <ian.field90@gmail.com>
2011-12-06 19:03:32 +0000

    Add concurrent counter example

---
+++ Part 3/Parking/src/csp/free/Counter.java
@@ -0,0 +1,32 @@
+package csp.free;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Counter {
+	private static AtomicLong number = new AtomicLong(0);
+	public static long increment(){
+		return number.getAndIncrement();
+	}
+	
+	public static void main(String argv[]){
+		
+		Thread t1 = new Thread(new Runnable(){
+			public void run(){
+				while(true){
+					System.out.println("T1 Increment: " + increment());
+				}
+			}
+		});
+		Thread t2 = new Thread(new Runnable(){
+			public void run(){
+				while(true){
+					System.out.println("T2 Increment: " + increment());
+				}
+			}
+		});
+	
+		t1.start();
+		t2.start();
+		
+	}
+}
