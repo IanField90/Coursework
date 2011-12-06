f4b245bfb0c18bf66c1cfdb003de846b1ba174d0
Ian Field <ian.field90@gmail.com>
2011-12-06 19:26:32 +0000

    Update to be more locky

--- Part 3/Parking/src/csp/free/Counter.java
+++ Part 3/Parking/src/csp/free/Counter.java
@@ -3,24 +3,24 @@ package csp.free;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Counter {
-	private static AtomicLong number = new AtomicLong(0);
-	public static long increment(){
-		return number.getAndIncrement();
-	}
+//	private static AtomicLong number = new AtomicLong(0);
+//	public static long increment(){
+//		return number.getAndIncrement();
+//	}
 	
 	public static void main(String argv[]){
-		
+		final AtomicLong number = new AtomicLong(0);
 		Thread t1 = new Thread(new Runnable(){
 			public void run(){
 				while(true){
-					System.out.println("T1 Increment: " + increment());
+					System.out.println("T1 Increment: " + number.getAndIncrement());//increment());
 				}
 			}
 		});
 		Thread t2 = new Thread(new Runnable(){
 			public void run(){
 				while(true){
-					System.out.println("T2 Increment: " + increment());
+					System.out.println("T2 Increment: " + number.getAndIncrement());//increment());
 				}
 			}
 		});
